165fc736ec3b8145020a009588b1c16d24d22b77
wechu07 <wechu017@gmail.com>
2024-01-01 21:07:15 +0300

    update header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,33 @@
 <script>
-    import "../app.css";
-  </script>
-  
+  import "../app.css";
+  import Footer from "../components/Footer.svelte";
+  import Navigation from "../components/Navigation.svelte";
+
+  let y = 0;
+  let innerHeight = 0;
+  let innerWidth = 0;
+
+  function goUp() {
+    document.body.scrollIntoView();
+  }
+</script>
+
+<div
+  class="relative flex flex-col max-w-[1400px] mx-auto w-full text-sm sm:text-base min-h-screen"
+>
+  <div
+    class={"fixed bottom-0 w-full duration-200 flex p-10 z-[10] " +
+      (y > 0
+        ? " opacity-full pointer-events-auto"
+        : " pointer-events-none opacity-0")}
+  >
+    <button on:click={goUp} class="ml-auto rounded-full bg-slate-900 text-violet-400 px-3 sm:px-4 hover:bg-slatw-800 cursor-pointer ">
+      <i class="fa-solid fa-arrow-up grid place-items-center aspect-square"></i>
+    </button>
+  </div>
+  <Navigation y={y}/>
   <slot />
+  <Footer />
+</div>
+
+<svelte:window bind:scrollY={y} bind:innerHeight bind:innerWidth />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a class="text-green-400" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<p>change</p>
+<script>
+    import Main from "../components/Main.svelte";
+</script>
+
+<Main />
